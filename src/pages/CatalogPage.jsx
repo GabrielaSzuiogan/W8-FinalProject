1515cf525a6052cc6fe8784adf5b6e532bc3d9ee
GabrielaSzuiogan <Gabriela.Szuiogan@cognizant.com>
2026-02-23 15:54:59 +0200

    structure: adding simple pages for project's structure

---
+++ src/pages/CatalogPage.jsx
@@ -0,0 +1,3 @@
+export default function CatalogPage() {
+  return <h1>The Grand Catalog</h1>;
+}
---
+++ src/pages/ProfilePage.jsx
@@ -0,0 +1,3 @@
+export default function ProfilePage() {
+  return <h1>Sprite Profile (and maybe his reading lists)</h1>;
+}
