5becb2a5ce3cd3ac4aaf798f98593fabc0f896bb
GabrielaSzuiogan <Gabriela.Szuiogan@cognizant.com>
2026-02-24 14:41:16 +0200

    refactor: added more global design vars

--- src/components/Layout.jsx
+++ src/components/Layout.jsx
@@ -1,13 +1,12 @@
 import { Outlet } from "react-router-dom";
 import { useSelector } from "react-redux";
 import Header from "./Header/Header";
-import "../index.css";
 
 export default function Layout() {
   const theme = useSelector((state) => state.ui.theme);
 
   return (
-    <div data-theme={theme} style={{ minHeight: "100vh" }}>
+    <div data-theme={theme} className="app-container">
       <Header />
       <main className="main-wrp">
         <Outlet />
